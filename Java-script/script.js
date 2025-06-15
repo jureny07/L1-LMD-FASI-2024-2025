90237e47b921e1dec9fff75a3f76d79fc25f4e25
jureny07 <jurenyamani43@gmail.com>
2025-06-15 20:18:42 +0100

    petit changement

---
+++ Java-script/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+  toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
